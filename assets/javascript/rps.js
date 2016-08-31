fab4bd425ee3feacb0fdd0a2e254847d5a80b51b
Houston Breedlove <wehaveaproblem16@Houstons-MacBook-Pro.local>
2016-08-31 16:00:33 -0400

    started game

---
+++ assets/javascript/rps.js
@@ -0,0 +1,101 @@
+$( document ).ready(function() {
+console.log( "ready!" );
+
+//----------------------------------
+var keySelections = ['r', 'p', 's'];
+//----------------------------------
+var player1Wins = 0;
+var player2Wins = 0;
+//----------------------------------
+var player1Losses = 0;
+var player2Losses = 0;
+//----------------------------------
+var ties = 0;
+//----------------------------------
+var player1Choice = "";
+var player2Choice = "";
+//----------------------------------
+// var console = function() {
+// 	console.log("Player 1 Wins: " + player1Wins);
+// 	console.log("Player 1 Losses: " + player1Losses);
+// 	console.log("Player 2 Wins: " + player2Wins);
+// 	console.log("Player 2 Losses: " + player2Losses);
+// }
+
+var decision = function() {
+	if ((player1Choice == 'rock') && (player2Choice == 'scissors')){
+			player1Wins++;
+			player2Losses++;
+			// console();
+		}else if ((player1Choice == 'rock') && (player2Choice == 'paper')){
+			player1Losses++;
+			player2Wins++;
+		}else if ((player1Choice == 'scissors') && (player2Choice == 'rock')){
+			player1Losses++;
+			player2Wins++;
+		}else if ((player1Choice == 'scissors') && (player2Choice == 'paper')){
+			player1Wins++;
+			player2Losses++;
+		}else if ((player1Choice == 'paper') && (player2Choice == 'rock')){
+			player1Wins++;
+			player2Losses++;
+		}else if ((player1Choice == 'paper') && (player2Choice == 'scissors')){
+			player1Losses++;
+			player2Wins++;
+		}else if (player1Choice == player2Choice){
+			ties++;
+		}
+}
+
+/* Player1 and Player2 both submit their choice */
+var player1Selection = function () {
+	$('#rock1').on('click', function() {
+		player1Choice = "rock";
+		console.log("Player 1 chose: " + player1Choice);
+		player2Selection();
+		$('#paper1').hide();
+		$('#scissors1').hide();
+	})
+	$('#paper1').on('click', function() {
+		player1Choice = "paper";
+		console.log("Player 1 chose: " + player1Choice);
+		player2Selection();
+		$('#rock1').hide();
+		$('#scissors1').hide();
+	})
+	$('#scissors1').on('click', function() {
+		player1Choice = "scissors";
+		console.log("Player 1 chose: " + player1Choice);
+		player2Selection();
+		$('#rock1').hide();
+		$('#paper1').hide();
+	})
+};
+
+player1Selection();
+
+var player2Selection = function () {
+	$('#rock2').on('click', function() {
+		player2Choice = "rock";
+		console.log("Player 1 chose: " + player2Choice);
+		$('#paper2').hide();
+		$('#scissors2').hide();
+	})
+	$('#paper2').on('click', function() {
+		player2Choice = "paper";
+		console.log("Player 1 chose: " + player2Choice);
+		$('#rock2').hide();
+		$('#scissors2').hide();
+	})
+	$('#scissors2').on('click', function() {
+		player2Choice = "scissors";
+		console.log("Player 1 chose: " + player2Choice);
+		$('#rock2').hide();
+		$('#paper2').hide();
+		decision();
+	})
+}
+
+
+
+});
